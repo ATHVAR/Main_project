d44c77733872c92885b1a1f7dcd5c5b4df63350a
ATHVAR <119851369+ATHVAR@users.noreply.github.com>
2023-08-12 17:27:09 +0530

    updatedapp.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,22 +1,13 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const jwt = require ('jsonwebtoken');
-
 const app = express();
 const PORT = 3000;
-
 app.use(bodyParser.json());
 app.use(cors());
-
 const db = require("../Backend/db/index");
 const api=require("../Backend/router/router");
 app.use('/',api);
-
-
-
-
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
